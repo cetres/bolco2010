dc803b4f976e7599326af44f609a35990c58b297
gustavo <gustavo@e411ec2e-46f0-4f65-aba2-99bd92c15ab1>
2014-06-11 00:38:03 +0000

    - atualizacao robo do Bwin

--- trunk/palpites/bolcoBwin.py
+++ trunk/palpites/bolcoBwin.py
@@ -21,7 +21,7 @@ def time_normalize(time):
     else:
         return time
 
-def download():
+def download(caminho):
     br = mechanize.Browser()
     logging.debug("Iniciando a conexao com o Bwin")
     br.open("https://sports.bwin.com/pt/sports")
@@ -37,6 +37,8 @@ def download():
     logging.debug(br.title())
     logging.debug(response1.geturl())
     nomeArq = 'bwin-' + time.strftime('%Y%m%d%H%M%S')
+    if caminho:
+        nomeArq = os.path.join(caminho,nomeArq)
     with open(nomeArq + ".head", 'w') as fp:
         fp.write(str(response1.geturl())+"\n")
         fp.write(str(response1.info()))
@@ -64,8 +66,12 @@ if __name__ == '__main__':
     parser = OptionParser(usage=usage)
     parser.add_option("-d", "--download", action="store_true", dest="download",
                   default=False, help=u"Realiza o download apenas da pagina")
+    parser.add_option("-c", "--caminho", dest="caminho", default=None,
+                  help=u"Caminho onde os arquivos serao trabalhados")
     parser.add_option("-a", "--arquivo", dest="arquivo", default=None,
                   help=u"Arquivo para leitura da fonte HTML. Nao havera download")
+    parser.add_option("-u", "--update", action="store_true", dest="update",
+                  default=False, help=u"Atualiza banco de dados")
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
                   help=u"Modo distrambelhado ;)")
     (options, args) = parser.parse_args()
@@ -76,14 +82,25 @@ if __name__ == '__main__':
         else:
             logging.basicConfig(level=logging.WARN)
         logging.debug("Logging configurado")
+        if options.caminho and not os.path.exists(options.caminho):
+            parser.error(u"Caminho nao encontrado - %s" % options.caminho)
         if options.arquivo:
             arquivo = options.arquivo.rstrip(".body")
+            if options.caminho:
+                if not os.path.isdir(options.caminho):
+                    parser.error(u"Caminho nao eh um diretorio - %s" % options.caminho)
+                arquivo = os.path.join(options.caminho, os.path.basename(arquivo))
             if not os.path.exists(arquivo + '.body'):
                 parser.error(u"Nao foi possivel localizar o arquivo - %s.body" % arquivo)
         else:
-            arquivo = download()
+            arquivo = download(options.caminho)
         if not options.download:
             logging.debug("Iniciando o processamento do arquivo")
             processa(arquivo)
+        if options.update:
+            import bolcoBwinProcess
+            dados = bolcoBwinProcess.carregar_arquivo(arquivo)
+            data = os.path.basename(arquivo)[5:13]
+            bolcoBwinProcess.atualizar_banco(dados, data)
     except:
         raise
